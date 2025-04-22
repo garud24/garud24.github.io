fa2f58aefb2a27f8c234a9ff31181d1e0b9a8261
garud24   <garudhimanshu4@gmail.com>
2025-04-22 03:58:48 -0400

    Updated resume and added projects

--- js/javascript.js
+++ js/javascript.js
@@ -41,7 +41,7 @@ window.onscroll = () => {
   document.getElementById(id).addEventListener("click", function (event) {
     event.preventDefault();
     if (confirm("Would you like to download my Resume?")) {
-      window.location.href = "Resume/HimanshuGarud.pdf";
+      window.location.href = "Resume/HimanshuKiranGarud.pdf";
     }
   });
 
